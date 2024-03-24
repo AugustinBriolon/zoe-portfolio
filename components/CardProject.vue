4cc10a977745f6382c26321126a91d83cb2b7ec5
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-24 23:07:36 +0100

    :sparkles: page projects done with filter

--- components/CardProject.vue
+++ components/CardProject.vue
@@ -4,10 +4,10 @@
     <img alt="" :src="`https:${projetData.media}`" class="h-auto w-full object-cover" />
     <div
       class="absolute inset-0 h-full w-full transition-all opacity-0 hover:opacity-100 bg-transparent hover:bg-gray-900/50 flex flex-col items-start justify-end">
-      <div class="p-4 sm:p-6 flex flex-col gap-2">
-        <p class="text-xs text-white uppercase">{{ formatDate(projetData.date) }}</p>
-        <h4 class="text-lg text-white line-clamp-2">{{ projetData.title }}</h4>
-        <p class="line-clamp-3 text-sm text-white">{{ projetData.description }}</p>
+      <div class="p-4 sm:p-6 flex flex-col gap-2 text-white">
+        <p class="text-xs uppercase">{{ formatDate(projetData.date) }}</p>  
+        <h4 class="text-lg font-bold line-clamp-2">{{ projetData.title }}</h4>
+        <p class="line-clamp-3 text-sm">{{ projetData.description }}</p>
       </div>
     </div>
   </NuxtLink>
--- pages/projets.vue
+++ pages/projets.vue
@@ -1,6 +1,21 @@
 <template>
-  <section class="h-screen-header flex flex-col gap-12 md:gap-8 items-center px-4">
+  <section class="h-fit flex flex-col gap-12 md:gap-8 items-center px-4">
     <h1>Projets</h1>
+    <div class="w-full">
+      <div class="flex gap-2 overflow-x-scroll no-scrollbar">
+        <Button name="ALL" class="uppercase whitespace-nowrap" :onClickFunction="() => filterProjects('all')" />
+        <Button v-for="(tag, index) in getTagsFromProjects" :key="index" :name="tag" class="uppercase whitespace-nowrap"
+          :onClickFunction="() => filterProjects(tag)" />
+      </div>
+    </div>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
+      <template v-for="(projects, index) in filteredProjects" :key="index">
+        <div class="flex flex-col gap-2">
+          <CardProject :projetData="projects" />
+        </div>
+      </template>
+    </div>
+
   </section>
 </template>
 
@@ -8,8 +23,44 @@
 export default {
   name: "Projets",
   data() {
-    return {};
+    return {
+      currentTag: null,
+    };
   },
-  methods: {},
+  computed: {
+    computedProjects() {
+      return useProjects().value;
+    },
+    filteredProjects() {
+      return this.computeFilteredProjects();
+    },
+    getTagsFromProjects() {
+      const projects = this.computedProjects;
+      let tags = projects.map((project) => project.tag);
+      tags = tags.map(tag => tag.replace(/([A-Z])/g, ' $1'));
+      return [...new Set(tags)];
+    },
+  },
+  methods: {
+    filterProjects(tag) {
+      if (tag === 'all') {
+        this.currentTag = null;
+        return;
+      }
+      this.currentTag = tag;
+    },
+    computeFilteredProjects() {
+      return this.currentTag
+        ? this.computedProjects.filter((project) => {
+          const formattedTag = project.tag.replace(/([A-Z])/g, ' $1');
+          return formattedTag === this.currentTag;
+        })
+        : this.computedProjects;
+    },
+
+  },
+  mounted() {
+    this.computedProjects;
+  },
 };
 </script>
